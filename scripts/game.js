931e193524bc902b934c1f084be24bca5f5b1dfa
rhysbobbett <rhys000bobbett@googlemail.com>
2023-05-17 11:37:19 +0000

    Add more tests

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,33 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
 }
 
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
 
-module.exports = { game };
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() =>{
+        document.getElementById(circ).classList.remove("light");
+    }, 400); 
+}    
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
